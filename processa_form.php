bbae48b1700b895ae0ec6c675e41e9d501df5657
joaovitorpereira5 <joaovitorpereiraquinto58@gmail.com>
2025-06-24 18:39:17 -0300

    Atualizando arquivos

--- processa_form.php
+++ processa_form.php
@@ -38,7 +38,7 @@ function renderHeader() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" />
-        <link rel="stylesheet" href="css/contato.css" />
+        <link rel="stylesheet" href="css/style.css" />
         <link rel="shortcut icon" href="imagens/icone.png" />
     </head>
     <body>
